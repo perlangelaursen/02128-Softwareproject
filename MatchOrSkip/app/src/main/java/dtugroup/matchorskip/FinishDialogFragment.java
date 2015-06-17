64daf2c3737b75953c8c177ef8a760d26a94f167
Per Lange Laursen <perlangelaursen@gmail.com>
2015-06-17 13:34:18 +0200

    Dialog cancel solved

--- MatchOrSkip/app/src/main/java/dtugroup/matchorskip/FinishDialogFragment.java
+++ MatchOrSkip/app/src/main/java/dtugroup/matchorskip/FinishDialogFragment.java
@@ -44,6 +44,7 @@ public class FinishDialogFragment extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(getString(R.string.gameover));
+        setCancelable(false);
         if (finishDialogListener.getCurrentScore() >= finishDialogListener.getLowestScore()) {
             if (finishDialogListener.getCurrentScore() > finishDialogListener.getHighestScore()) {
                 builder.setMessage(getString(R.string.congratulations) + "\n\n" +
