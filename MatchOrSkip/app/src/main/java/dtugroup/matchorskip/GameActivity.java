df91f3785308767d4aa352a59f4561f3f1d83df6
anhvann <Van Anh Thi Trinh>
2015-06-17 16:17:43 +0200

    Bonus card chooser working

--- MatchOrSkip/app/src/main/java/dtugroup/matchorskip/GameActivity.java
+++ MatchOrSkip/app/src/main/java/dtugroup/matchorskip/GameActivity.java
@@ -6,7 +6,6 @@ import android.graphics.Matrix;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
