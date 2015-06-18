7531a7f29a66fd4eb7b664eb748da579722565e8
anna0207 <annaoelgaardnielsen@hotmail.com>
2015-06-18 13:10:39 +0200

    highscore work - 0 points is not highscore

--- MatchOrSkip/app/src/main/java/dtugroup/matchorskip/FinishDialogFragment.java
+++ MatchOrSkip/app/src/main/java/dtugroup/matchorskip/FinishDialogFragment.java
@@ -49,7 +49,8 @@ public class FinishDialogFragment extends DialogFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(getString(R.string.gameover));
         setCancelable(false);
-        if (finishDialogListener.getCurrentScore() >= finishDialogListener.getLowestScore()) {
+        if (finishDialogListener.getCurrentScore() >= finishDialogListener.getLowestScore()
+                && finishDialogListener.getCurrentScore()!=0) {
             if (finishDialogListener.getCurrentScore() > finishDialogListener.getHighestScore()) {
                 builder.setMessage(getString(R.string.congratulations) + "\n\n" +
                         getString(R.string.youscored) + finishDialogListener.getCurrentScore()
