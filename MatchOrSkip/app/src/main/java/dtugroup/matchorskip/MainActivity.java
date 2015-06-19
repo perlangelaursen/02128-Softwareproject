876ecd94a7de9cf7135de336c05ae563835e50b1
Per Lange Laursen <perlangelaursen@gmail.com>
2015-06-19 10:19:36 +0200

    Font changed in MainActivity

--- MatchOrSkip/app/src/main/java/dtugroup/matchorskip/MainActivity.java
+++ MatchOrSkip/app/src/main/java/dtugroup/matchorskip/MainActivity.java
@@ -2,18 +2,30 @@ package dtugroup.matchorskip;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
+    private Button playbutton, instructions, highscoreButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playbutton = (Button) findViewById(R.id.playbutton);
+        playbutton.setTypeface(Typeface.createFromAsset(getAssets(), "ComingSoon.ttf"));
+
+        instructions = (Button) findViewById(R.id.instructions);
+        instructions.setTypeface(Typeface.createFromAsset(getAssets(), "ComingSoon.ttf"));
+
+        highscoreButton = (Button) findViewById(R.id.highscoreButton);
+        highscoreButton.setTypeface(Typeface.createFromAsset(getAssets(), "ComingSoon.ttf"));
     }
 
     @Override
