b4d2bfe1bb64fae57db034eaf9c4072bc54c340a
Per Lange Laursen <perlangelaursen@gmail.com>
2015-06-21 10:39:18 +0200

    Refactored VerifyFragment

--- MatchOrSkip/app/src/main/java/dtugroup/matchorskip/VerifyFragment.java
+++ MatchOrSkip/app/src/main/java/dtugroup/matchorskip/VerifyFragment.java
@@ -66,44 +66,76 @@ public class VerifyFragment extends Fragment {
         }
 
         private void imageMatch(String[] params) {
-            if(params[0].toLowerCase().equals("bonus")) {
+            if(isBonusOrRush(params[0], "bonus")) {
                 addPoints = 5;
                 right = true;
                 gameActivity.playSound(4);
-            } else if (params[0].toLowerCase().equals("rush")){
+            } else if (isBonusOrRush(params[0], "rush")){
                 gameActivity.setRushTime();
                 gameActivity.playSound(4);
             } else {
-                if(gameActivity.getRushTime()){
-                    if(params[0].toLowerCase().equals("keep") || params[0].toLowerCase().equals("skip")){
-                        addPoints = 1;
-                        right = false;
-                        gameActivity.playSound(2);
-                    }
+                if(isRushTime()){
+                    rushTimeCheck(params[0]);
                 } else {
-                    if (params[0].toLowerCase().equals("keep") &&
-                            idMatch(data[0].getID(), data[1].getID())) {
-                        addPoints = 1;
-                        right = true;
-                        gameActivity.playSound(2);
-                    } else if (params[0].toLowerCase().equals("skip") &&
-                            idMatch(data[0].getID(), data[1].getID())) {
-                        addPoints = 0;
-                        right = false;
-                        gameActivity.playSound(1);
-                    } else if (params[0].toLowerCase().equals("keep") &&
-                            !idMatch(data[0].getID(), data[1].getID())) {
-                        addPoints = -1;
-                        right = false;
-                        gameActivity.playSound(3);
-                    } else if (params[0].toLowerCase().equals("skip") &&
-                            !idMatch(data[0].getID(), data[1].getID())) {
-                        addPoints = 0;
-                        right = false;
-                        gameActivity.playSound(1);
-                    }
+                    regularImageCheck(params[0]);
                 }
             }
+        }
+
+        private boolean isRushTime() {
+            return gameActivity.getRushTime();
+        }
+
+        private void rushTimeCheck(String input) {
+            if(isBonusOrRush(input, "keep") || isBonusOrRush(input, "skip")){
+                addPoints = 1;
+                right = false;
+                gameActivity.playSound(2);
+            }
+        }
+
+        private void regularImageCheck(String input) {
+            if (isKeepAndCardMatch(input)) {
+                addPoints = 1;
+                right = true;
+                gameActivity.playSound(2);
+            } else if (isSkipAndCardMatch(input)) {
+                addPoints = 0;
+                right = false;
+                gameActivity.playSound(1);
+            } else if (isKeepAndCardDoesNotMatch(input)) {
+                addPoints = -1;
+                right = false;
+                gameActivity.playSound(3);
+            } else if (isSkipAndCardDoesNotMatch(input)) {
+                addPoints = 0;
+                right = false;
+                gameActivity.playSound(1);
+            }
+        }
+
+        private boolean isSkipAndCardDoesNotMatch(String input) {
+            return isBonusOrRush(input, "skip") &&
+                    !idMatch(data[0].getID(), data[1].getID());
+        }
+
+        private boolean isKeepAndCardDoesNotMatch(String input) {
+            return isBonusOrRush(input, "keep") &&
+                    !idMatch(data[0].getID(), data[1].getID());
+        }
+
+        private boolean isSkipAndCardMatch(String input) {
+            return isBonusOrRush(input, "skip") &&
+                    idMatch(data[0].getID(), data[1].getID());
+        }
+
+        private boolean isKeepAndCardMatch(String input) {
+            return isBonusOrRush(input, "keep") &&
+                    idMatch(data[0].getID(), data[1].getID());
+        }
+
+        private boolean isBonusOrRush(String input, String compare) {
+            return input.toLowerCase().equals(compare);
         }
 
         @Override
